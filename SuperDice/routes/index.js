3875f7d668b54cf9a7e986230bd38688a4da3145
Rocky <y001j@hotmail.com>
2018-10-09 11:52:47 +0800

    change a little

--- SuperDice/routes/index.js
+++ SuperDice/routes/index.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
-let eos = require('./common.js').eos;
+//let eos = require('./common.js').eos;
 var bodyParser = require('body-parser');
 
 /* GET home page. */
@@ -14,39 +14,39 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.post('/rolldice', function (req, res) {
     var content = req.body;//加密的明文；
     //console.log(content.ownerkey + content.activekey);
-    console.log(content.account);
+    //console.log(content.account);
    
 
-    eos.transaction(
-        {
-            actions: [
-                {
-                    account: 'eosio.token',
-                    name: 'transfer',
-                    authorization: [{
-                        actor: content.account,
-                        permission: 'active'
-                    }],
-                    data: {
-                        from: content.account,
-                        to: 'chinaplayers',
-                        quantity: content.quantity + ' EOS',
-                        memo: content.memo
-                    }
-                }
-            ]
-        }
-        //options
-    ).then(trx => {
-        console.log("get siged transaction data: ", trx);
-        console.log('completed.');
-        res.send('success');
-    }).catch(
-        e => {
-            console.log("error", e);
-            res.send('transaction failed');
-        }
-    );
+    //eos.transaction(
+    //    {
+    //        actions: [
+    //            {
+    //                account: 'eosio.token',
+    //                name: 'transfer',
+    //                authorization: [{
+    //                    actor: content.account,
+    //                    permission: 'active'
+    //                }],
+    //                data: {
+    //                    from: content.account,
+    //                    to: 'chinaplayers',
+    //                    quantity: content.quantity + ' EOS',
+    //                    memo: content.memo
+    //                }
+    //            }
+    //        ]
+    //    }
+    //    //options
+    //).then(trx => {
+    //    console.log("get siged transaction data: ", trx);
+    //    console.log('completed.');
+    //    res.send('success');
+    //}).catch(
+    //    e => {
+    //        console.log("error", e);
+    //        res.send('transaction failed');
+    //    }
+    //);
 });
 
 module.exports = router;
